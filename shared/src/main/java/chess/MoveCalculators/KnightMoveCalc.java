bd3fc3a93a019ea9834c92c0ea6deafa64529936
sethfrand <sethp.frandsen@gmail.com>
2025-04-30 10:37:17 -0600

    only pawn left, all others pass

--- shared/src/main/java/chess/MoveCalculators/KnightMoveCalc.java
+++ shared/src/main/java/chess/MoveCalculators/KnightMoveCalc.java
@@ -21,51 +21,36 @@ public class KnightMoveCalc {
                     {1,-2}, // up 1 left 2
                     {2,-1} //down 2, left 1
             };
-    public static HashSet<chess.ChessMove>getMoves(chess.ChessBoard board, ChessPosition position)
-    {
+    public static HashSet<chess.ChessMove>getMoves(chess.ChessBoard board, ChessPosition position) {
         HashSet<ChessMove> validMove = new HashSet<>();
-        ChessPiece piece  = board.getPiece(position);
+        ChessPiece piece = board.getPiece(position);
 
-        if(piece == null) // base case for if the piece isnt there
-        {
+        if (piece == null) {
             return validMove;
         }
-
-        for (int []direction : Directions) // check all directions
+        for (int[] direction : Directions) // check all possible moves
         {
             int rowInc = direction[0];
             int colInc = direction[1];
 
-            // move diagonally until you reach the edge
             int currRow = position.getRow() + rowInc;
             int currCol = position.getColumn() + colInc;
 
-            while(isValidMove(currRow,currCol)) //can move more than one space
-            {
+            if (isValidMove(currRow, currCol)) {
                 ChessPosition newPos = new ChessPosition(currRow, currCol);
                 ChessPiece atDest = board.getPiece(newPos);
 
                 //If the square that the piece wants to move to is empty, update the position
                 if (atDest == null) //this is a valid move
                 {
-                    validMove.add(new ChessMove(position,newPos,null));}
-                else if(atDest.getTeamColor() != piece.getTeamColor())
-                {
                     validMove.add(new ChessMove(position, newPos, null));
-                    break;
+                } else if (atDest.getTeamColor() != piece.getTeamColor()) {
+                    validMove.add(new ChessMove(position, newPos, null)); //in the future this is where we would add a point to the other team
                 }
-                else
-                {
-                    break; //else the move is invalid
-                }
-                //incrementing the direction that we are moving
-                currRow += rowInc;
-                currCol+= colInc;
             }
         }
         return validMove;
     }
-    //create helper function that can check to see if the move is valid
     static boolean isValidMove(int row, int col)
     {
         return row >= 1 && row <= 8 && col >= 1 && col <= 8;
