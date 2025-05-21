cdfe3d3366b35db140d154cd5aa2187bef231f49
sethfrand <sethp.frandsen@gmail.com>
2025-05-21 16:58:45 -0600

    refactored knight and king to utilize the new moveCalc class. QUALITY CHECK

--- shared/src/main/java/chess/movecalculators/KnightMoveCalc.java
+++ shared/src/main/java/chess/movecalculators/KnightMoveCalc.java
@@ -1,5 +1,6 @@
 package chess.movecalculators;
 
+import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -22,35 +23,8 @@ public class KnightMoveCalc {
             };
 
     public static HashSet<chess.ChessMove> getMoves(chess.ChessBoard board, ChessPosition position) {
-        HashSet<ChessMove> validMove = new HashSet<>();
-        ChessPiece piece = board.getPiece(position);
-
-        if (piece == null) {
-            return validMove;
-        }
-        for (int[] direction : DIRECTIONS) // check all possible moves
-        {
-            int rowInc = direction[0];
-            int colInc = direction[1];
-
-            int currRow = position.getRow() + rowInc;
-            int currCol = position.getColumn() + colInc;
-
-            if (isValidMove(currRow, currCol)) {
-                ChessPosition newPos = new ChessPosition(currRow, currCol);
-                ChessPiece atDest = board.getPiece(newPos);
-
-                //If the square that the piece wants to move to is empty, update the position
-                if (atDest == null) //this is a valid move
-                {
-                    validMove.add(new ChessMove(position, newPos, null));
-                } else if (atDest.getTeamColor() != piece.getTeamColor()) {
-                    validMove.add(new ChessMove(position, newPos, null)); //in the future this is where we would add a point to the other team
-                }
-            }
-        }
-        return validMove;
-    }//helper function that can check to see if the move is valid
+        return MoveCalculator.single(board, position, DIRECTIONS);
+    }
 
     static boolean isValidMove(int row, int col) {
         return row >= 1 && row <= 8 && col >= 1 && col <= 8;
