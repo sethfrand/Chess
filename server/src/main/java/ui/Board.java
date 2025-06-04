9f991d0195f3a32ecdc78f1356610af33de1d981
sethfrand <sethp.frandsen@gmail.com>
2025-06-03 21:38:19 -0600

    Have boarders and written for white and black perspectives. Working on rows

--- server/src/main/java/ui/Board.java
+++ server/src/main/java/ui/Board.java
@@ -1,5 +1,9 @@
 package ui;
 
+import chess.ChessBoard;
+import chess.ChessGame;
+import static ui.EscapeSequences.*;
+
 public class Board {
     """
     have board size
@@ -10,7 +14,50 @@ public class Board {
 
     set to see if it will be from perspective of white or black
 
-
-    
 """
+
+
+    private static final int BOARD_SIZE = 8;
+    private static final String SIDE_CHARS = "  a b c d e f g h  ";
+
+
+    public static void printBoard(ChessBoard board, ChessGame.TeamColor perspective)
+    {
+        if(perspective == ChessGame.TeamColor.WHITE)
+        {
+            printWhiteBoard(board);
+        }else{
+            printBlackBoard(board);
+        }
+    }
+
+    public static void printWhiteBoard(ChessBoard board)
+    {
+        System.out.print(ERASE_SCREEN);
+
+        System.out.println(SET_BG_COLOR_BLACK + SET_BG_COLOR_WHITE + SIDE_CHARS + RESET_BG_COLOR);
+
+        for (int row = 1; row <= 8; row++ )
+        {
+            printWhitekRow(board, row);
+        }
+
+        System.out.println(SET_BG_COLOR_BLACK + SET_BG_COLOR_WHITE + SIDE_CHARS + RESET_BG_COLOR);
+        System.out.println();
+    }
+
+    public static void printBlackBoard(ChessBoard board)
+    {
+        System.out.print(ERASE_SCREEN);
+
+        String reverseSideChars = "  h g f e d c b a  ";
+        System.out.println(SET_BG_COLOR_BLACK + SET_BG_COLOR_WHITE + reverseSideChars + RESET_BG_COLOR);
+        for (int row = 1; row <=8; row++)
+        {
+            printBlackRow(board, row);
+        }
+        System.out.println(SET_BG_COLOR_BLACK + SET_BG_COLOR_WHITE + SIDE_CHARS + RESET_BG_COLOR);
+        System.out.println();
+    }
+
 }
