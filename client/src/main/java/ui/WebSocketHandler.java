c5533ab7a18b035c72bea19d858a337eca1bec2b
sethfrand <sethp.frandsen@gmail.com>
2025-06-05 20:33:25 -0600

    websocket handler created

---
+++ client/src/main/java/ui/WebSocketHandler.java
@@ -0,0 +1,12 @@
+package ui;
+
+import chess.*;
+import com.google.gson.Gson;
+import model.GameData;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+
+@WebSocket
+public class WebSocketHandler {
+    private static 
+}
